7e6f769366364d1a3fc929badd23299a7a57542f
Ashrita <ashrita.karna@gmail.com>
2022-04-18 11:32:00 +0530

    Added else Condition for dccgap

--- src/support/deal_builder/dcc_gap_existingVendor.command.js
+++ src/support/deal_builder/dcc_gap_existingVendor.command.js
@@ -10,7 +10,16 @@ Cypress.Commands.add("existingVendorForDCCAndGAP", (insuranceVendor, vendorName,
     //perform enter vendor,enter dealer cost,enter price
     // if insurance is None
     //else enter vendor, dealer cost
-    if (insuranceVendor === "DCC") {
+    if (insuranceVendor === "DCC" || insuranceVendor === "GAP") {
+        cy.get("button").contains(" DCC/GAP ").click();
+        cy.get(`.modal-content input[formcontrolname='insuranceProtectionType'][value='${insuranceVendor}']`).check();
+        cy.wait(1000);
+        cy.get("select[formcontrolname='vendor_id']").select(`${vendorName}`);
+        //  .select(`${vendorName}`);
+        cy.get("input[formcontrolname='dealer_cost']").type(`${dealerCost}`);
+        cy.get("input[formcontrolname='insuranceDCCSalePrice']").type(`${costPrice}`);
+        // cy.get("input[formcontrolname='insuranceProtectionType']").find("input[value='GAP']").click();
+        cy.get("button").contains("SAVE").click();
 
     }
     cy.get("button").contains(" DCC/GAP ").click();
@@ -19,7 +28,7 @@ Cypress.Commands.add("existingVendorForDCCAndGAP", (insuranceVendor, vendorName,
     cy.get("select[formcontrolname='vendor_id']").select(`${vendorName}`);
     //  .select(`${vendorName}`);
     cy.get("input[formcontrolname='dealer_cost']").type(`${dealerCost}`);
-    cy.get("input[formcontrolname='insuranceDCCSalePrice']").type(`${costPrice}`);
+    // cy.get("input[formcontrolname='insuranceDCCSalePrice']").type(`${costPrice}`);
     // cy.get("input[formcontrolname='insuranceProtectionType']").find("input[value='GAP']").click();
     cy.get("button").contains("SAVE").click();
 })
