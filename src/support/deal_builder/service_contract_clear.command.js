974e6a12884c1d35dd3707e32120000a624fc135
shashi ersani <shashigoud.e@gmail.com>
2022-04-19 00:53:04 +0530

    clearing the service contract

---
+++ src/support/deal_builder/service_contract_clear.command.js
@@ -0,0 +1,13 @@
+/// <reference types ="Cypress"/>
+
+     Cypress.Commands.add("serviceContractClear", () =>{
+     
+
+     //clearing service contract
+     cy.get("button").contains("Service Contract").click();
+     cy.contains("Vehicle Service Contract(VSC)").should("be.visible");
+     cy.get("button").contains(" Clear Service Contract ").click();
+     cy.wait(1000);
+     cy.get("input[formcontrolname = 'Warranty']").invoke("val").should("contain", "0.00");
+
+     });
