02db9185e0a38d03a9548a4ab02dfcf067e389ce
shashi ersani <shashigoud.e@gmail.com>
2022-04-19 10:53:41 +0530

    added the clear contract

--- src/features/sales/servicecontract.spec.js
+++ src/features/sales/servicecontract.spec.js
@@ -64,10 +64,11 @@ describe("Sales Test cases", () => {
 
     //newvendor for service contract
     cy.newVendorForServiceContract(randomCompanyName);
-// cy.wait(5000);
-// cy.ClearServiceContract();
+     //ClearServiceContract
+ cy.wait(5000);
+ cy.ClearServiceContract();
 
-    //ClearServiceContract
+   
 
     //SAGI VSC
     //cy.get(`.modal-content input[formcontrolname='warrantyType'][value="SAGI VSC"]`).check();
