f91faf999a52a9370095f59a2ab5c85772cb6b92
Ashrita <ashrita.karna@gmail.com>
2022-04-20 13:15:14 +0530

    small changes in tescase

--- src/features/sales/cash_sale/Testcase_dwnpmt_tradein.spec.js
+++ src/features/sales/cash_sale/Testcase_dwnpmt_tradein.spec.js
@@ -5,30 +5,30 @@ var customerInfos = require(`../../data/customer_info.${ENV}.json`);
 var quotationDetails = require(`../../data/quotation_detail.json`);
 
 describe("Dealbuilder Testcases", () => {
-    cy.login();
-    //Take Phone number from Json file and populate in  Phone field
-    const customer = customerInfos[0];
-    cy.lookupExitingCustomer(customer);
-    //Click on Next button
-    cy.get("app-customer-info button").contains("NEXT").click();
-    //assertion for popup
-    cy.contains(" Verify Customer Information").should("be.visible");
-    //click on Confirm in the Dialog box
-    //Intercept
-    cy.intercept(`${API_URL}/inventory/list?staged=staged`).as("vehicleList");
-    cy.get("modal-container button").contains("Confirm").click();
-    //Wait
-    cy.wait("@vehicleList");
-    // In Select vehicle Tab-check condition for vehicles>0 (a.vehicle-select) this have length>0
-    cy.get("a.vehicle-select").its("length").should("be.greaterThan", 0);
-    // array.first(); it selects first vehicle
-    cy.intercept(`${API_URL}/sales/credit700/*`).as("dealBuilder");
-    cy.get("a.vehicle-select").first().click();
-    cy.wait("@dealBuilder", { timeout: 10000 });
-    //check with Api call if the page is moved to Dealbuilder page
-    cy.contains("Quotation Detail").should("be.visible");
-    it("Check Sales Price,salestax,Other charges  by TradeIn and Downpayment", () => {
 
+    it("Check Sales Price,salestax,Other charges  by TradeIn and Downpayment", () => {
+        cy.login();
+        //Take Phone number from Json file and populate in  Phone field
+        const customer = customerInfos[0];
+        cy.lookupExitingCustomer(customer);
+        //Click on Next button
+        cy.get("app-customer-info button").contains("NEXT").click();
+        //assertion for popup
+        cy.contains(" Verify Customer Information").should("be.visible");
+        //click on Confirm in the Dialog box
+        //Intercept
+        cy.intercept(`${API_URL}/inventory/list?staged=staged`).as("vehicleList");
+        cy.get("modal-container button").contains("Confirm").click();
+        //Wait
+        cy.wait("@vehicleList");
+        // In Select vehicle Tab-check condition for vehicles>0 (a.vehicle-select) this have length>0
+        cy.get("a.vehicle-select").its("length").should("be.greaterThan", 0);
+        // array.first(); it selects first vehicle
+        cy.intercept(`${API_URL}/sales/credit700/*`).as("dealBuilder");
+        cy.get("a.vehicle-select").first().click();
+        cy.wait("@dealBuilder", { timeout: 10000 });
+        //check with Api call if the page is moved to Dealbuilder page
+        cy.contains("Quotation Detail").should("be.visible");
         //dealbuilder
         const quotation = quotationDetails[2];
 
