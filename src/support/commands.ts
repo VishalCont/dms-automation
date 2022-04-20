e63d0f1b7b7bc9de06abbdc2f66a2dcbd7290ff8
George <george.peter@contenterra.com>
2022-04-20 17:24:55 +0530

    index of deffe&downpaymet

--- src/support/commands.ts
+++ src/support/commands.ts
@@ -14,6 +14,8 @@ require("./deal_builder/service_contract_existingVendor.command");
 require("./deal_builder/service_contract_newVendor.command");
 require("./deal_builder/clear_service_contract.command");
 require("./deal_builder/remove_tradein.command")
+require("./deal_builder/clear_downpayment.command");
+require("./deal_builder/clear_defferdownpayment.command");
 // add new command to the existing Cypress interface
 declare global {
   namespace Cypress {
