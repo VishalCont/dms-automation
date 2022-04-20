ad0951f2d6b1fbc690d36ccd504dcebfa748954f
George <george.peter@contenterra.com>
2022-04-20 11:33:47 +0530

    chnges done on json,service contractand done testcase4

--- src/support/deal_builder/service_contract_existingVendor.command.js
+++ src/support/deal_builder/service_contract_existingVendor.command.js
@@ -2,17 +2,20 @@
 
 const { getRandomNumber } = require("../../utils/random_number");
 
-
-Cypress.Commands.add("existingVendorForServiceContract", () =>{
-
+Cypress.Commands.add(
+  "existingVendorForServiceContract",
+  (dealerMarkup, cogs) => {
     //Service contract button for existing vendor
     cy.get("button").contains("Service Contract").click();
     cy.contains("Vehicle Service Contract(VSC)").should("be.visible");
     //service contract
     cy.get("select[formcontrolname='vendor_id']").select("Dc company");
-    cy.get("input[formcontrolname = 'dealer_mark_up']").type(getRandomNumber(100, 200));
-    cy.get("input[formcontrolname = 'cogs']").type(getRandomNumber(100, 200));
+    cy.get("input[formcontrolname = 'dealer_mark_up']").type(dealerMarkup);
+    cy.get("input[formcontrolname = 'cogs']").type(cogs);
     cy.get("button").contains(" SAVE & CONTINUE ").click();
     cy.wait(2000);
-    cy.get("input[formcontrolname = 'Warranty']").invoke("val").should("not.be.empty");
-})
+    cy.get("input[formcontrolname = 'Warranty']")
+      .invoke("val")
+      .should("not.be.empty");
+  }
+);
